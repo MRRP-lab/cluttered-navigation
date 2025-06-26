50c0abcbcd85038b0a7ac3b92b026065df877b44
Graham Shupe <116042260+grahamshupe@users.noreply.github.com>
2025-06-26 13:35:07 -0700

    finished merge?

--- pinko-drones/scenes/Initialize.gd
+++ pinko-drones/scenes/Initialize.gd
@@ -1,7 +1,13 @@
 extends Node2D
 class_name Swarm_Sim
 
- 
+
+@export var obstacle_shape := "triangle"  ## Shape of the obstacle field, either 'triangle' or 'rectangle'
+@export var obstacle_width := 4  ## Number of obstacles along the y-axis of the field. If triangular, the width of the base
+@export var obstacle_depth := 4  ## Number of obstacles along the x-axis of the field. If triangular, this is ignored
+@export var obstacle_spacing := 60  ## Spacing in pixels between obstacles
+@export_range(0, 100, 1, "suffix:%") var obstacle_randomization: int  ## Degree of randomization. Higher value -> larger randomization offset
+
 @export var droneNum: int
 
 @export_range(1, 180, 1, "suffix:°") var Minimum_Angle: float
@@ -13,6 +19,12 @@ class_name Swarm_Sim
 # Optionally initialize the board.
 func _ready() -> void:
 	print("Hello World!")
+	var start_point = obstacles.position + Vector2(140, 0)
+	if obstacle_shape == "triangle":
+		generate_triangle(start_point)
+	else:
+		generate_rectangle(start_point)
+	
 	# Initialize drones in start region based on the parameters.
 	var drone = preload("res://scenes/plinko_drone.tscn")
 	for i in range(droneNum):
@@ -27,4 +39,38 @@ func _ready() -> void:
 		drones.add_child(newDrone)
 		print("Drone sprite texture:", newDrone.get_node("Sprite2D").texture)
 		print("Drone position", newDrone.position)
+
+
+## Generates obstacles in a triangular plinko formation
+func generate_triangle(start_point: Vector2) -> void:
+	print("Generating obstacles in a triangular plinko formation")
+	
+	var obstacle = preload("res://scenes/obstacle.tscn")
+	for row in obstacle_width:
+		for col in row + 1:
+			var new_obstacle = obstacle.instantiate()
+			new_obstacle.name = str("obstacle_", row, col)
+			
+			new_obstacle.position.x = start_point.x + (row * obstacle_spacing)
+			new_obstacle.position.y = start_point.y + (col * obstacle_spacing) - (row * obstacle_spacing / 2)
+			obstacles.add_child(new_obstacle)
+
+## Generates obstacles in a rectangular plinko formation
+## The start vector corresponds to the middle of the first row of obstacles
+func generate_rectangle(start_point: Vector2) -> void:
+	print("Generating obstacles in a rectangular formation")
+	
+	var obstacle = preload("res://scenes/obstacle.tscn")
+	for row in obstacle_depth:
+		var width = obstacle_width
+		if (row % 2):
+			width -= 1
 		
+		for col in width:
+			var new_obstacle = obstacle.instantiate()
+			new_obstacle.name = str("obstacle_", row, col)
+			
+			new_obstacle.position.x = start_point.x + (row * obstacle_spacing)
+			new_obstacle.position.y = start_point.y + (col * obstacle_spacing) - (width * obstacle_spacing) / 2 + (obstacle_spacing / 2)
+			obstacles.add_child(new_obstacle)
+			
