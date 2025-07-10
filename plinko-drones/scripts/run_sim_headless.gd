9bf5f0a3999d4c90bab54ebab4b7c09daa90de7b
Alli <anilles137@gmail.com>
2025-07-09 20:37:11 -0700

    cleanup

--- plinko-drones/scripts/run_sim_headless.gd
+++ plinko-drones/scripts/run_sim_headless.gd
@@ -1,32 +1,8 @@
 extends SceneTree
 
 var scene_path := "res://scenes/field.tscn"
-var position_arg := Vector2.ZERO
-var velocity_arg := Vector2.ZERO
-
-func reset(angle: float, weighted=true):
-    var scene = load("res://scenes/field.tscn")
-    # Instance the new scene.
-    var instance = scene.instantiate()
-
-    # Add it to the active scene, as child of root.
-    # get_tree().root.add_child(instance)
-
-    # Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
-    # get_tree().instance = instance
-
-    instance.obstacle_width = 6
-    instance.obstacle_depth = 6
-    instance.droneNum = 20
-    instance.weighted_direction = true
-    instance.Maximum_Angle = angle
-    var w = "UW"
-    if weighted:
-        w = "W"
-    # set logging filename based on variable values in this instance
-    instance.filename_ee = "sample-N"+str(instance.droneNum)+"-"+w+"-ee"
-    instance.filename_cont = "sample-N"+str(instance.droneNum)+"-"+w+"-cont"
-    #change_scene_to_packed(instance)
+var N_arg := 20
+var w_arg := false
 
 func parse_command_line_args():
     var args = OS.get_cmdline_args()
@@ -36,59 +12,48 @@ func parse_command_line_args():
         match args[i]:
             "-w":
                 print("weighted")
+            "-N":
                 if i + 1 < args.size():
-                    scene_path = args[i + 1]
-            "--pos":
-                if i + 2 < args.size():
-                    position_arg = Vector2(args[i + 1].to_float(), args[i + 2].to_float())
-            "--vel":
-                if i + 2 < args.size():
-                    velocity_arg = Vector2(args[i + 1].to_float(), args[i + 2].to_float())
+                    N_arg = args[i + 1].to_int()
 
-func load_and_run_simulation():
+func load_and_run_simulation(angle: float, weighted=true):
+    # load base scene to godot
     var scene_res = load(scene_path)
     if not scene_res:
         push_error("Failed to load scene: " + scene_path)
         quit(1)
-
     var scene_instance = scene_res.instantiate()
     if not scene_instance:
         push_error("Failed to instance scene.")
         quit(1)
-
     root.add_child(scene_instance)
+    var sim = root.get_node("Simulator")
 
-        # Assuming the simulation has a node named "Agent" to modify
-    var sim = scene_instance.get_node("Simulator") if scene_instance.has_node("Simulator") else null
-    if sim:
-        sim.droneNum = 5
-    #if sim.has_method("set_velocity"):
-    #    sim.set_velocity(velocity_arg)
-    #elif "velocity" in sim:
-    #    sim.velocity = velocity_arg
+    # DEFAULTS
+    ###
+    sim.droneNum = 5
+    sim.obstacle_width = 6
+    sim.obstacle_depth = 6
 
-    # Optionally call a method like "start" to run the simulation
-    if scene_instance.has_method("start_simulation"):
-        scene_instance.start_simulation()
+    # SET FOR THIS SIMRUN
+    ###
+    sim.droneNum = N_arg
+    sim.weighted_direction = w_arg
+    sim.Maximum_Angle = angle
+    var w = "uw"
+    if weighted:
+        sim.weighted_direction = true
+        w = "w"
+
+    # SET LOGS
+    ###
+    # set logging filename based on variable values in this sim
+    sim.filename_ee = "sample-N"+str(sim.droneNum)+"-"+w+"-ee"
+    sim.filename_cont = "sample-N"+str(sim.droneNum)+"-"+w+"-cont"
+    #change_scene_to_packed(sim) # not needed?
 
 func _init():
     parse_command_line_args()
-    load_and_run_simulation()
-
-#func _init():
-#
-#    # get command line arguments (flags)
-#    # no idea why this stopped working
-#    var args = Array(OS.get_cmdline_args())
-#    print(args)
-#
-#    # change variable values based on flags
-#    if args.has("-w"):
-#        print("weighted")
-#    else:
-#        print("unweighted")
-#
-#    # loop over variable(s) of interest based on flags
-#    reset(45.0)
-#    # run sim with logging
-#    quit()
+    var angle = 40
+    var weighted = true
+    load_and_run_simulation(angle, weighted)
