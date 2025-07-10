b82623bb5e8cb4e432f47f5dc689b330a98190b2
Alli <anilles137@gmail.com>
2025-07-09 22:44:44 -0700

    tried to fix whitespace and I broke everything. adding example real logs from back when they were working

--- plinko-drones/scripts/run_sim_headless.gd
+++ plinko-drones/scripts/run_sim_headless.gd
@@ -1,6 +1,7 @@
 extends SceneTree
 
 var scene_path := "res://scenes/field.tscn"
+# var Init = preload("res://scripts/Initialize.gd").new()
 var N_arg := 20
 var w_arg := false
 
@@ -16,7 +17,7 @@ func parse_command_line_args():
                 if i + 1 < args.size():
                     N_arg = args[i + 1].to_int()
 
-func load_and_run_simulation(angle: float, weighted=true):
+func load_and_run_simulation(angle: float, N: int, weighted=true):
     # load base scene to godot
     var scene_res = load(scene_path)
     if not scene_res:
@@ -31,13 +32,14 @@ func load_and_run_simulation(angle: float, weighted=true):
 
     # DEFAULTS
     ###
-    sim.droneNum = 5
+
     sim.obstacle_width = 6
     sim.obstacle_depth = 6
+    sim.log_button.emit_signal("pressed")
 
     # SET FOR THIS SIMRUN
     ###
-    sim.droneNum = N_arg
+    sim.droneNum = N
     sim.weighted_direction = w_arg
     sim.Maximum_Angle = angle
     var w = "uw"
@@ -48,12 +50,24 @@ func load_and_run_simulation(angle: float, weighted=true):
     # SET LOGS
     ###
     # set logging filename based on variable values in this sim
-    sim.filename_ee = "sample-N"+str(sim.droneNum)+"-"+w+"-ee"
-    sim.filename_cont = "sample-N"+str(sim.droneNum)+"-"+w+"-cont"
-    #change_scene_to_packed(sim) # not needed?
+    var filename_ee = "sample-N"+str(sim.droneNum)+"-"+w+"-ee"
+    var filename_cont = "sample-N"+str(sim.droneNum)+"-"+w+"-cont"
+    sim.filename_ee = filename_ee # idk anymore
+    sim.filename_cont = filename_cont
+    var filepath_ee = "res://output-data/" + filename_ee + ".txt"
+    var filepath_cont = "res://output-data/" + filename_cont + ".txt"
+
+    print("Logging")
+    # makes empty files? how to run in loop?
+    sim.logger.log_data_ee(filepath_ee)
+    sim.logger.log_data_cont(filepath_cont)
+
+    # timeout and quit cleanly?
 
 func _init():
     parse_command_line_args()
-    var angle = 40
+    var angle := 40.0
+    var N := 5
     var weighted = true
-    load_and_run_simulation(angle, weighted)
+    load_and_run_simulation(angle, N, weighted)
+    quit()
