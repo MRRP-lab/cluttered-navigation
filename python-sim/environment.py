115fbf1d8aaa9982d689e9c3d70cc237b7a1540c
Mrnibbles <parkerg444@gmail.com>
2026-02-23 12:59:51 -0800

    Make the top and bottom of the screen be barriers, add plinko obstacle generation.

--- python-sim/environment.py
+++ python-sim/environment.py
@@ -9,7 +9,8 @@ class Environment():
         self.grid_num = grid_num
         self.x_coords = np.arange(0, self.grid_num, dtype=float)
         self.seed = seed
-        self.obstacles = self.generate_random_obstacles(0.1)
+        self.obstacles = self.generate_plinko_grid(5, 1, 2, 0)
+        # self.obstacles = self.generate_random_obstacles(0.1)
 
     def generate_random_obstacles(self, density):
         rng = np.random.default_rng(self.seed)
@@ -19,9 +20,26 @@ class Environment():
                 obstacles[i] = 1
         return np.reshape(obstacles, (self.grid_num, self.grid_num))
 
+    def generate_plinko_grid(self, start_col, row_gap, pin_gap, noise):
+        obstacles = np.full(self.grid_num**2, 0)
+        obstacles = np.reshape(obstacles, (self.grid_num, self.grid_num))
+        col = start_col
+        offset = 0
+        while col < obstacles.shape[0]:
+            if col % (row_gap + 1) != 0:
+                col += 1
+                continue
+            for row in range(obstacles.shape[1]):
+                if (row + offset) % (pin_gap + 1) != 0:
+                    continue
+                obstacles[row, col] = 1
+            col += 1
+            offset = (offset + 1) % (pin_gap + 1)
+        return obstacles
+
     def is_obstacle(self, x, y):
         if (x >= self.obstacles.shape[0] or x < 0):
             return 0
         if (y >= self.obstacles.shape[1] or y < 0):
-            return 0
+            return 1
         return self.obstacles[y, x]
--- python-sim/params.py
+++ python-sim/params.py
@@ -2,9 +2,9 @@ class Params:
     FPS = 55
     time_seconds = 10
     sim_time = time_seconds*FPS
-    gridnum = 25 # num of grid cells along one side of the window
-    cell_size = 20 # Size of a grid cell in px
+    gridnum = 75 # num of grid cells along one side of the window
+    cell_size = 10 # Size of a grid cell in px
     ss = gridnum * cell_size  # screen size (px)
-    N = 1
+    N = 10
     v = 1  # velocity
     seed = 1
