16724218af26be422f15f8f15299ce456c1372ab
Madden-R <noodlerota@gmail.com>
2026-02-23 18:27:01 -0800

    add log_line() to DataLogger
    also initialize the export_data() function.

--- python-sim/data_logger.py
+++ python-sim/data_logger.py
@@ -2,3 +2,11 @@ class DataLogger():
 
     def __init__(self, outputFilepath):
         self.outputFilepath = outputFilepath
+        self.data = []
+    
+    def log_line(self, droneID: int, timestep: int, xPos: int, yPos: int):
+        line = str(droneID) + "," + str(timestep) + "," + str(xPos) + "," + str(yPos)
+        self.data.append(line)
+    
+    def export_data():
+        pass
