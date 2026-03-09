264ca25eb355269485edef4541ba2f0535cf970c
Madden-R <noodlerota@gmail.com>
2026-03-09 11:49:59 -0700

    Merged main into my branch

--- python-sim/params.py
+++ python-sim/params.py
@@ -4,10 +4,10 @@ class Params:
     FPS = 55
     time_seconds = 10
     sim_time = time_seconds*FPS
-    gridnum = 25 # num of grid cells along one side of the window
+    gridnum = 50 # num of grid cells along one side of the window
     cell_size = 20 # Size of a grid cell in px
     ss = gridnum * cell_size  # screen size (px)
-    N = 5
+    N = 15
     v = 1  # velocity
     seed = 1
 
