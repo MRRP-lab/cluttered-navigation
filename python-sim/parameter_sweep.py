ceda052e3d78c71e5ac80055e3ab6700b68a9dc5
Mrnibbles <parkerg444@gmail.com>
2026-03-16 14:57:05 -0700

    Finish parameter sweeping (untested)

--- python-sim/parameter_sweep.py
+++ python-sim/parameter_sweep.py
@@ -1,5 +1,9 @@
+import os
 import sys
+import subprocess
 import itertools
+from concurrent.futures import ProcessPoolExecutor
+
 
 params = {
         "N": [50, 100],
@@ -7,6 +11,8 @@ params = {
         "boundary_angle": [22.5, 20, 15],
         "experiment_name": "test 1"
         }
+
+simulator = "./generate_demo.py"
 
 # If we don't do this, itertools.product will treat strings as a list of characters.
 normalized = {
@@ -20,3 +26,28 @@ values = normalized.values()
 combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
 print(combinations)
 
+def run_single_sim(params):
+    args = [sys.executable, simulator]
+    for k, v in params.items():
+        args.extend([f"--{k}", str(v)])
+    try:
+        result = subprocess.run(
+                args,
+                capture_output=True,
+                text=True,
+                check=False
+                )
+
+        return {
+                "exit_code": result.returncode,
+                "stderr": result.stderr if result.returncode != 0 else "",
+                "status": "SUCCESS" if result.returncode == 0 else "FAILED"
+                }
+    except Exception as e:
+        return {**params, "status": "CRASHED", "err": str(e)}
+
+def run_sweep(all_combinations):
+    with ProcessPoolExecutor() as executor:
+        results = list(executor.map(run_single_sim, all_combinations))
+
+    return results
