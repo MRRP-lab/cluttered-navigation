0f905c6e115aa10884d0c11b67f992c120eb37d7
Parker Gutierrez <parkerg444@gmail.com>
2026-03-17 21:15:29 -0700

    Compute traversal times

--- analytics/generate_analytics.py
+++ analytics/generate_analytics.py
@@ -54,32 +54,44 @@ def main():
 # Traversal statistics for each drone.
 # Finish line makespan
 def compute_analytics(playback_path, params_path):
-    raw_playback_data = pd.read_csv(playback_path)
+    playback_data = pd.read_csv(playback_path)
     with open(params_path) as f:
         params = yaml.safe_load(f)
-    print(raw_playback_data)
-    playback_data = raw_playback_data
     data = {}
+
     compute_traversal_stats(playback_data, params, data)
-    compute_makespan(playback_data, data)
     compute_EMD(playback_data, data)
     return data
 
 # Given the playback, params, and data, computes various statistics related to traversal and
 # adds them to data.
 def compute_traversal_stats(playback, params, data):
-    compute_traversal(playback, data, params.num)
+    compute_traversal(playback, data, params)
+    compute_makespan(playback, data, params)
     pass
 # Entry and exit times per drone. Helps with makespan
-# [start_time, y], [finish_time, y]
+# {'entry': [start_time, y], 'exit': [finish_time, y]}
 # Given the playback data, add traversal times for each drone to data.
-def compute_traversal(playback, data, num):
-    traversal = []
-    pass
+def compute_traversal(playback, data, params):
+    data["traversal"] = []
+    starts = playback[playback["x"] == 0]
+    finishes = playback[playback["x"] == params["gridnum"]]
+
+    # Left join so we can detect when a robot starts but never finishes.
+    merged = starts.merge(finishes, on="id", how="left", suffixes=("_entry", "_exit"))
+    merged = merged.fillna(-1)
+    data["traversal"] = [
+            {
+                "id": row.id,
+                "entry": [row.time_entry, row.y_entry],
+                "exit": [row.time_exit, row.y_exit]
+            }
+            for row in merged.itertuples()
+     ]
 
 # Single number, the finishing makespan
 # Given the playback data and data containing traversal info, add the finish line makespan to data.
-def compute_makespan(playback, num, data):
+def compute_makespan(playback, data, num):
     pass
 
 # Given the playback data, add earth movers distance per timestep to data.
