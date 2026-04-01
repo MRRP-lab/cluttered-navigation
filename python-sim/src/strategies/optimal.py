d060988542f6b57e14572b476b94380604bf9b51
Parker Gutierrez <parkerg444@gmail.com>
2026-04-01 14:00:19 -0700

    Send and receive test protobuf messages.

--- python-sim/src/strategies/optimal.py
+++ python-sim/src/strategies/optimal.py
@@ -1,4 +1,5 @@
 import socket
+import src.protobuf.multiagent_pathfinding_protobuf.problem_pb2 as problem
 
 from src.strategies.navstrategy import NavStrategy
 
@@ -13,8 +14,28 @@ class OptimalStrategy(NavStrategy):
     def run(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((OptimalStrategy.ip, OptimalStrategy.port))
-        # wait for a response and for the socket to be closed. Yield in the meantime.
-        pass
+        assignment_msg = problem.Assignment()
+        assignment_msg.robot_id = 100
+        assignment_msg.start_id = 123
+        assignment_msg.finish_id = 500
+
+        message = assignment_msg.SerializeToString()
+        self.socket.sendall(message)
+        self.socket.shutdown(socket.SHUT_WR)
+
+        # wait for a response to be completed...
+        resp = b""
+        while chunk := self.socket.recv(4096):
+            resp += chunk
+        self.socket.close()
+        assignment_resp = problem.Assignment()
+        assignment_resp.ParseFromString(resp)
+
+        print(f"Python: {assignment_resp.robot_id}")
+        print(f"Python: {assignment_resp.start_id}")
+        print(f"Python: {assignment_resp.finish_id}")
+
 
     def extract_data(self):
         pass
+
